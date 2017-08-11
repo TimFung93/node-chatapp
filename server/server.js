79270cb72abc5e857b2df7a35cb80449344280c1
timfung93 <timothy.fung.04@gmail.com>
2017-08-11 16:40:48 -0400

    added event listeners for reveiving message

--- server/server.js
+++ server/server.js
@@ -1,18 +1,49 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
-
+const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+
 
 app.use(express.static(publicPath))
 
-app.get('/', (req,res) => {
-	res.render('index.html')
+//lets you resgister an event listener
+io.on('connection', (socket) => {
+	console.log('New user connected')
+
+	//specify data to the event listener
+
+
+	socket.on('createMessage', (messageData) => {
+		console.log('new message data:', messageData)
+		io.emit('newMessage', {
+			from: messageData.from,
+			text: messageData.text,
+			createdAt: new Date().getTime()
+		})
+	});
+
+	socket.on('disconnect', () => {
+		console.log('User was disconnected from server')
+	})
 });
 
 
-app.listen(port, () => {
+
+
+
+// app.get('/', (req,res) => {
+// 	res.render('index.html')
+// });
+
+
+server.listen(port, () => {
 	console.log(`Server is up on http://localhost:${port}`)
 });
