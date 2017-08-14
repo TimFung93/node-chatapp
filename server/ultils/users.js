505a3b3f122ee054fb88e4b6649b1ea524d0c01f
timfung93 <timothy.fung.04@gmail.com>
2017-08-14 17:08:36 -0400

    added users databases/methods

---
+++ server/ultils/users.js
@@ -0,0 +1,54 @@
+class Users {
+	constructor() {
+		this.users = []
+	}
+	addUser(id, name, room) {
+		const user = {
+			id,
+			name,
+			room
+		}
+		this.users.push(user)
+		return user
+	}
+
+	removeUser(id) {
+
+		const removedUser = this.getUser(id)
+		if (removedUser) {
+			this.users = this.users.filter(user => {
+				return user.id !== id
+			})
+		}
+
+		return removedUser
+	}
+
+	getUser(id) {
+		const user = this.users.filter(user => {
+			return user.id === id
+		})
+
+		return user[0]
+	}
+
+	getUserList(room) {
+		const users = this.users.filter(user => {
+			return user.room === room
+		})
+
+		const userList = users.map(user => {
+			return user.name
+		})
+
+		return userList
+	}
+
+};
+
+
+
+
+module.exports = {
+	Users
+};
