c018f23267b47c2fa59197d481da451ddd8d3db9
timfung93 <timothy.fung.04@gmail.com>
2017-08-25 19:14:48 -0400

    added test

---
+++ server/tests/removeDup.test.js
@@ -0,0 +1,7 @@
+const expect = require('expect');
+
+const {removeDup} = require('../ultils/removeDup');
+
+describe('removeDup function', () => {
+	it('Should create only accept arrays as the parameter')
+});
